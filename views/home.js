e904c44f69c162966fb58171d153d4301d5b2142
Jordan <jordanwyldebrowne@gmail.com>
2023-07-06 18:54:42 +1000

    lots of changes
    add spa support
    add projects list
    add footer
    refactor styling

--- views/home.js
+++ views/home.js
@@ -1,5 +1,5 @@
 let html = `
-		<p class="big-text">Hi, I'm Jordan.</br></br> I am an undergraduate Comp Sci student from Tasmania.</br></br> This is my portfolio website where some of the things I've worked on can be found.</p>
+		<p class="big-text">Hi, I'm Jordan</br></br> I'm an undergraduate Comp Sci student from Tasmania, Australia</br></br> This is my portfolio website where some of the things I've worked on can be found</p>
 	`;
 // display the home page content
 
--- views/projects.js
+++ views/projects.js
@@ -3,12 +3,19 @@ import data from "../projects-list.json";
 
 let html = `<ul class="projects-list">`;
 data.forEach((val) => {
-    html += `<li class="project-tile">`;
-    html += `<img src="../assets/${val.img}"></img><div class="project-tile-desc">`;
+    html += `<li class="project-tile-wrapper"><div class="project-tile">`;
+    // html += `<img src="../assets/${val.img}"></img><div class="project-tile-desc">`;
+    // html += `<div class="project-tile-desc">`;
     html += `<h1>${val.title}</h1>`;
-    html += `<p>${val.full_text}</p>`;
+    html += `<p>${val.short_text}</p>`;
     html += "</div></li>";
 });
 html += "</ul>";
+// <li class="project-tile-wrapper">
+//     <div class="project-tile">
+//         <h1>portfolio website</h1>
+//         <p>this website, with vanillia html/css/js</p>
+//     </div>
+// </li>;
 
 export default () => html;

cd442fc54c7e96eafa045212beb6cdb472dce914
Jordan <jordanwyldebrowne@gmail.com>
2023-07-14 12:11:52 +1000

    final fixes for spa

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/",
-    appType: "mpa", // disable history fallback
+    build: {
+        rollupOptions: {
+            input: {
+                index: "index.html",
+                404: "404.html",
+                // Add more entries for each HTML file you want in your app
+            },
+        },
+    },
 });
